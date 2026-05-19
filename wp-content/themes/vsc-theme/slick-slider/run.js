4d3af89698a32c5cfec22a30c373e2c0f19a23d6
asma <asma@viruscomm.ca>
2026-05-19 15:37:48 +0100

    home page

--- wp-content/themes/vsc-theme/slick-slider/run.js
+++ wp-content/themes/vsc-theme/slick-slider/run.js
@@ -15,7 +15,7 @@ jQuery(document).on('ready', function() {
                 settings: {
                     arrows: true,
                     centerMode: true,
-                    centerPadding: '55px',
+                    centerPadding: '460px',
                     slidesToShow: 1
                 }
             },
