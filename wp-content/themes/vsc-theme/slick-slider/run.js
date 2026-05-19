b8afc159969bf20ff1e3366e9fe748205d2514f3
asma <asma@viruscomm.ca>
2026-05-19 09:05:24 +0100

    slider

--- wp-content/themes/vsc-theme/slick-slider/run.js
+++ wp-content/themes/vsc-theme/slick-slider/run.js
@@ -2,7 +2,7 @@ jQuery(document).on('ready', function() {
     jQuery(".cd-gallery-slider > .vce-row-content").slick({
         centerMode: true,
         centerPadding: '55px',
-        slidesToShow: 3,
+        slidesToShow: 2,
         autoplay: false,
         autoplaySpeed: 20000,
         infinite:  true, 
