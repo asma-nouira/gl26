2a724276ce292cd579982c49d93cb39a0c553bbb
asma <asma@viruscomm.ca>
2026-05-21 14:39:14 +0100

    pages

--- wp-content/themes/vsc-theme/functions.php
+++ wp-content/themes/vsc-theme/functions.php
@@ -309,8 +309,12 @@ add_shortcode('cd_blog_grid', function($atts) {
 
         $title     = get_the_title();
         $permalink = get_the_permalink();
-        $excerpt   = get_the_excerpt();
+        /* Remplacez cette ligne : */
+$excerpt   = get_the_excerpt();
 
+/* Par : */
+$excerpt = wp_trim_words(get_the_excerpt(), 20, '...');
+/* 20 = nombre de mots — ajustez selon le rendu */
         $thumb = has_post_thumbnail()
             ? get_the_post_thumbnail(get_the_ID(), 'large', array('class' => 'cd-blog-card-img'))
             : '<div class="cd-blog-card-no-img"></div>';
